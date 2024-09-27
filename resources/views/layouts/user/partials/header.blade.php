e5bdd117198aa0de56b2eb890155267ae2888e1b
Jan Eris Saludo <janerissaludo@gmail.com>
2024-09-27 23:24:01 +0800

    Revert "Merge main branch to harv branch"

--- resources/views/layouts/user/partials/header.blade.php
+++ resources/views/layouts/user/partials/header.blade.php
@@ -2,8 +2,7 @@
     <nav class="flex justify-between space-x-4 h-full mx-8">
 
         <div class="flex space-x-4 items-center">
-            <img src="{{asset('images/logo.png')}}" class="w-14 " alt="" srcset="">
-                
+            <div class="bg-red-500 rounded-full w-10 h-10"></div>        
             <div class="font-medium text-white"> 
                 <h1>PENRO System</h1>
                 <p>Boac Santol Marinduque</p>
